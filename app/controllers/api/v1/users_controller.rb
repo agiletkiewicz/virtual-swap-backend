02eca7a4ae899e87510f0542ffeea012c34d3a6d
agiletkiewicz <a.letkiewicz@gmail.com>
2020-09-15 13:52:16 -0400

    generate user serializer

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,9 +1,23 @@
 class Api::V1::UsersController < ApplicationController
 
     def index
+        users = User.all
+        render json: UserSerializer.new(users)
     end
 
     def create
+        user = User.new(user_params)
+        if user.save 
+            render json: UserSerializer.new(user), status: :accepted
+        else
+            render json: {errors: event.errors.full_messages}, status: :unprocessible_entity
+        end
     end
-    
+
+    private 
+
+    def user_params 
+        params.require(:user).permit(:name, :event_id)
+    end
+
 end
