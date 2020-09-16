a73de179d75b4670e93a1aafac6ff7f9551ef4ea
agiletkiewicz <a.letkiewicz@gmail.com>
2020-09-16 16:40:52 -0400

    debugging item create

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -4,6 +4,7 @@ class Api::V1::ItemsController < ApplicationController
     def create 
         item = Item.new(item_params)
         if item.save 
+            # render json: ItemSerializer.new(item), status: :accepted
             render json: item, status: :accepted
         else
             render json: {errors: item.errors.full_messages}, status: :unprocessible_entity
