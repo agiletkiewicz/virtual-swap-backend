882940f7d5cfd55705fa36b2b8105915b207066d
agiletkiewicz <a.letkiewicz@gmail.com>
2020-09-17 18:19:31 -0400

    destroy action and route for takes

--- app/controllers/api/v1/takes_controller.rb
+++ app/controllers/api/v1/takes_controller.rb
@@ -3,11 +3,15 @@ class Api::V1::TakesController < ApplicationController
     def create 
         take = Take.new(take_params)
         if take.save 
-            # render json: ItemSerializer.new(item), status: :accepted
             render json: take, status: :accepted
         else
             render json: {errors: take.errors.full_messages}, status: :unprocessible_entity
         end
+    end
+
+    def destroy
+        take = Take.find_by(id: params[:id])
+        take.destroy
     end
 
     def take_params 
