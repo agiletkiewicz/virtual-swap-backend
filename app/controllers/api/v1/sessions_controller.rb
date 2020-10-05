809dd3bce5c2f06466c9cba7e113d7ed92b83d28
agiletkiewicz <a.letkiewicz@gmail.com>
2020-10-04 20:32:53 -0700

    add authenticate to event lologin

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -2,7 +2,7 @@ class Api::V1::SessionsController < ApplicationController
 
     def create 
         event = Event.find_by(id: params[:id])
-        if event && event.pin == params[:pin]
+        if event && event.authenticate(params[:password])
             redirect_to api_v1_event_path(event)
         else
             render json: {message: "Incorrect event pin."}
